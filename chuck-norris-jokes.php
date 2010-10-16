34650550751b7d3f80eada3ac36c23de157ff40e
maarten.decat <maarten.decat@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-10-16 20:48:43 +0000

    first version
    git-svn-id: https://plugins.svn.wordpress.org/chuck-norris-joke-widget/trunk@300741 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ chuck-norris-jokes.php
@@ -0,0 +1,132 @@
+<?php
+/**
+ * Plugin Name: Chuck Norris Jokes Widget
+ * Plugin URI: http://maartendecat.be/chuck-norris-jokes-wordpress-plugin/
+ * Description: A widget that shows Chuck Norris jokes on your blog. 
+ * Version: 0.1
+ * Author: Maarten Decat
+ * Author URI: http://maartendecat.be
+ * License: GPL2
+ *
+ * Copyright 2010  Maarten Decat  (email : mdecat@ulyssis.be)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License, version 2, as 
+ * published by the Free Software Foundation.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+add_action( 'widgets_init', 'load_ChuckNorrisWidgets' );
+
+function load_ChuckNorrisWidgets() {
+	register_widget( 'ChuckNorrisWidget' );
+}
+
+/**
+ * The Chuck Norris Widget.
+ */
+class ChuckNorrisWidget extends WP_Widget {
+
+	/**
+	 * Widget setup.
+	 */
+	function ChuckNorrisWidget() {
+		/* Widget settings. */
+		$widget_ops = array( 'classname' => 'chuck-norris-jokes', 'description' => __('A widget that shows Chuck Norris jokes on your blog.', 'chuck-norris-jokes-widget') );
+
+		/* Widget control settings. */
+		$control_ops = array( 'width' => 300, 'height' => 350, 'id_base' => 'chuck-norris-jokes-widget' );
+
+		/* Create the widget. */
+		parent::WP_Widget( 'chuck-norris-jokes-widget', __('Chuck Norris Jokes Widget', 'chuck-norris-jokes-widget'), $widget_ops, $control_ops );
+	}
+
+	/**
+	 * How to display the widget on the screen.
+	 */
+	function widget( $args, $instance ) {
+		extract( $args );
+
+		/* Our variables from the widget settings. */
+		$firstName = $instance['firstName'];
+		$lastName = $instance['lastName'];
+		$useName = $instance['useName'];
+
+		/* Before widget (defined by themes). */
+		echo $before_widget;
+
+		/* Display the widget title if one was input (before and after defined by themes). */
+		//if ( $title )
+		//	echo $before_title . $title . $after_title;
+
+		$hardCodedQuote = "In the Bible, Jesus turned water into wine. But then $firstName $lastName turned that wine into beer.";
+		
+		$file = fopen ('http://chuck.maartendecat.be/api.php?f=rand&fn=' . $firstName . '&ln=' . $lastName, "r");
+		if (!$file) {
+			echo($hardCodedQuote);
+		}
+		$result = '';
+		while (!feof ($file)) {
+			$result .= fgets ($file, 1024);
+		}
+		fclose($file);
+		$result = json_decode($result);
+		if($result->type == 'success') {
+			echo($result->value->quote);
+		} else {
+			echo($hardCodedQuote);
+		}
+
+		/* After widget (defined by themes). */
+		echo $after_widget;
+	}
+
+	/**
+	 * Update the widget settings.
+	 */
+	function update( $new_instance, $old_instance ) {
+		$instance = $old_instance;
+
+		/* Strip tags for title and name to remove HTML (important for text inputs). */
+		$instance['firstName'] = strip_tags( $new_instance['firstName'] );
+		$instance['lastName'] = strip_tags( $new_instance['lastName'] );
+
+		return $instance;
+	}
+
+	/**
+	 * Displays the widget settings controls on the widget panel.
+	 * Make use of the get_field_id() and get_field_name() function
+	 * when creating your form elements. This handles the confusing stuff.
+	 */
+	function form( $instance ) {
+
+		/* Set up some default widget settings. */
+		$defaults = array('firstName' => 'Chuck', 'lastName' => 'Norris');
+		$instance = wp_parse_args( (array) $instance, $defaults ); ?>
+
+		Want to star in your own jokes? Fill in your name below! If you want the original Chuck Norris jokes, just click save.
+
+		<!-- First name: Text Input -->
+		<p>
+			<label for="<?php echo $this->get_field_id( 'firstName' ); ?>">First name:</label>
+			<input id="<?php echo $this->get_field_id( 'firstName' ); ?>" name="<?php echo $this->get_field_name( 'firstName' ); ?>" value="<?php echo $instance['firstName']; ?>" style="width:100%;"/>
+		</p>
+
+		<!-- Last name: Text Input -->
+		<p>
+			<label for="<?php echo $this->get_field_id( 'lastName' ); ?>">Last name:</label>
+			<input id="<?php echo $this->get_field_id( 'lastName' ); ?>" name="<?php echo $this->get_field_name( 'lastName' ); ?>" value="<?php echo $instance['lastName']; ?>" style="width:100%;"/>
+		</p>
+<?php
+	}
+}
+
+?>
---
+++ readme.txt
@@ -0,0 +1,44 @@
+=== Plugin Name ===
+Contributors: maarten.decat
+Donate link: http://maartendecat.be
+Tags: chuck norris jokes
+Requires at least: 2.8
+Tested up to: 3.0.1
+Stable tag: 4.3
+
+Shows a random Chuck Norris joke on your blog, possibly starring yourself.
+
+== Description ==
+
+This plugin adds a small Wordpress widget that shows a random Chuck Norris joke on your blog.
+These jokes are taken from a database on chuck.maartendecat.be. It is also possible to change
+the name of the main character in the joke to star in it yourself!
+
+== Installation ==
+
+To install this plugin:
+
+1. Upload `chuck-norris-jokes.php` to the `/wp-content/plugins/` directory
+1. Activate the plugin through the 'Plugins' menu in WordPress
+1. Drag the "Chuck Norris Joke" widget in a widget area through the 'Appearance - Widgets' menu.
+1. Adjust the name of the main character in the joke or keep the default settings. Then press save.
+
+You should now see a different Chuck Norris joke on your blog every time the page loads.
+
+== Frequently Asked Questions ==
+
+Have a question? Be sure to ask it at [the plugin homepage](http://maartendecat.be/chuck-norris-jokes-wordpress-plugin "Chuck Norris Jokes Wordpress Plugin Homepage")
+
+== Screenshots ==
+
+1. A Chuck Norris joke.
+
+== Changelog ==
+
+= 0.1 =
+* Basic version: a random joke with possibility to show own first name and last name in the joke (basic replacement).
+
+== Upgrade Notice ==
+
+= 0.1 =
+Basic version.
