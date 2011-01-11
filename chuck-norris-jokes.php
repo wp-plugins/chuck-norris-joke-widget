84e9b2799afcc0e9770ba013826e3593c29c2b31
maarten.decat <maarten.decat@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-01-11 17:13:16 +0000

    updated to v0.5
    git-svn-id: https://plugins.svn.wordpress.org/chuck-norris-joke-widget/trunk@331322 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- chuck-norris-jokes.php
+++ chuck-norris-jokes.php
@@ -2,8 +2,8 @@
 /**
  * Plugin Name: Chuck Norris Jokes Widget
  * Plugin URI: http://maartendecat.be/chuck-norris-jokes-wordpress-plugin/
- * Description: A widget that shows Chuck Norris jokes on your blog, possibly starring yourself.
- * Version: 0.1.5
+ * Description: A widget that shows Chuck Norris jokes on your blog. For personalized Chuck Norris jokes starring yourself, please refer to the Personalized Chuck Norris Jokes Widget.
+ * Version: 0.5
  * Author: Maarten Decat
  * Author URI: http://maartendecat.be
  * License: GPL2
@@ -23,21 +23,21 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
-add_action( 'widgets_init', 'load_ChuckNorrisWidgets' );
+add_action( 'widgets_init', 'load_ChuckNorrisJokeWidget' );
 
-function load_ChuckNorrisWidgets() {
-	register_widget( 'ChuckNorrisWidget' );
+function load_ChuckNorrisJokeWidget() {
+	register_widget( 'ChuckNorrisJokeWidget' );
 }
 
 /**
  * The Chuck Norris Widget.
  */
-class ChuckNorrisWidget extends WP_Widget {
+class ChuckNorrisJokeWidget extends WP_Widget {
 
 	/**
 	 * Widget setup.
 	 */
-	function ChuckNorrisWidget() {
+	function ChuckNorrisJokeWidget() {
 		/* Widget settings. */
 		$widget_ops = array( 'classname' => 'chuck-norris-jokes', 'description' => __('A widget that shows Chuck Norris jokes on your blog.', 'chuck-norris-jokes-widget') );
 
@@ -45,7 +45,22 @@ class ChuckNorrisWidget extends WP_Widget {
 		$control_ops = array( 'width' => 300, 'height' => 350, 'id_base' => 'chuck-norris-jokes-widget' );
 
 		/* Create the widget. */
-		parent::WP_Widget( 'chuck-norris-jokes-widget', __('Chuck Norris Jokes Widget', 'chuck-norris-jokes-widget'), $widget_ops, $control_ops );
+		parent::WP_Widget( 'chuck-norris-jokes-widget', __('Chuck Norris Jokes', 'chuck-norris-jokes-widget'), $widget_ops, $control_ops );
+	}
+
+	/**
+	 *	Returns a joke
+	 */
+	function getHardcodedJoke() {
+		$firstName = 'Chuck';
+		$lastName = 'Norris';
+		$jokes = array(
+			"In the Bible, Jesus turned water into wine. But then $firstName $lastName turned that wine into beer.",
+			"Time waits for no man. Unless that man is $firstName $lastName.",
+			"If you spell $firstName $lastName in Scrabble, you win. Forever.",
+			"The Great Wall of China was originally created to keep $firstName $lastName out. It failed miserably.",
+			"While urinating, $firstName $lastName is easily capable of welding titanium.");
+		return $jokes[rand(0,count($jokes)-1)];
 	}
 
 	/**
@@ -54,35 +69,29 @@ class ChuckNorrisWidget extends WP_Widget {
 	function widget( $args, $instance ) {
 		extract( $args );
 
-		/* Our variables from the widget settings. */
-		$firstName = $instance['firstName'];
-		$lastName = $instance['lastName'];
-		$useName = $instance['useName'];
-
 		/* Before widget (defined by themes). */
 		echo $before_widget;
 
-		/* Display the widget title if one was input (before and after defined by themes). */
-		//if ( $title )
-		//	echo $before_title . $title . $after_title;
-
-		$hardCodedQuote = "In the Bible, Jesus turned water into wine. But then $firstName $lastName turned that wine into beer.";
+		/* Leave a signature */
+		echo('<!-- Chuck Norris Joke Widget plugin -->');
 		
-		$file = fopen ('http://icndb.com/api.php?f=rand&fn=' . $firstName . '&ln=' . $lastName, "r");
+		/* Output the quote */
+		$file = fopen ('http://api.icndb.com/jokes/random?client=2', "r");
 		if (!$file) {
-			echo($hardCodedQuote);
-		}
-		$result = '';
-		while (!feof ($file)) {
-			$result .= fgets ($file, 1024);
+			echo($this->getHardcodedJoke($firstName, $lastName));
+		} else {
+			$result = '';
+			while (!feof ($file)) {
+				$result .= fgets ($file, 1024);
+			}
+			$result = json_decode($result);
+			if($result != null && $result->type == 'success') {
+				echo($result->value->joke);
+			} else {
+				echo($this->getHardcodedJoke($firstName, $lastName));
+			}
 		}
 		fclose($file);
-		$result = json_decode($result);
-		if($result->type == 'success') {
-			echo($result->value->quote);
-		} else {
-			echo($hardCodedQuote);
-		}
 
 		/* After widget (defined by themes). */
 		echo $after_widget;
@@ -94,10 +103,6 @@ class ChuckNorrisWidget extends WP_Widget {
 	function update( $new_instance, $old_instance ) {
 		$instance = $old_instance;
 
-		/* Strip tags for title and name to remove HTML (important for text inputs). */
-		$instance['firstName'] = strip_tags( $new_instance['firstName'] );
-		$instance['lastName'] = strip_tags( $new_instance['lastName'] );
-
 		return $instance;
 	}
 
@@ -107,25 +112,7 @@ class ChuckNorrisWidget extends WP_Widget {
 	 * when creating your form elements. This handles the confusing stuff.
 	 */
 	function form( $instance ) {
-
-		/* Set up some default widget settings. */
-		$defaults = array('firstName' => 'Chuck', 'lastName' => 'Norris');
-		$instance = wp_parse_args( (array) $instance, $defaults ); ?>
-
-		Want to star in your own jokes? Fill in your name below! If you want the original Chuck Norris jokes, just click save.
-
-		<!-- First name: Text Input -->
-		<p>
-			<label for="<?php echo $this->get_field_id( 'firstName' ); ?>">First name:</label>
-			<input id="<?php echo $this->get_field_id( 'firstName' ); ?>" name="<?php echo $this->get_field_name( 'firstName' ); ?>" value="<?php echo $instance['firstName']; ?>" style="width:100%;"/>
-		</p>
-
-		<!-- Last name: Text Input -->
-		<p>
-			<label for="<?php echo $this->get_field_id( 'lastName' ); ?>">Last name:</label>
-			<input id="<?php echo $this->get_field_id( 'lastName' ); ?>" name="<?php echo $this->get_field_name( 'lastName' ); ?>" value="<?php echo $instance['lastName']; ?>" style="width:100%;"/>
-		</p>
-<?php
+		
 	}
 }
 
