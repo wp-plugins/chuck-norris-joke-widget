b492d98fd6b4e81edc192baa64d6ad97d6ae0317
maarten.decat <maarten.decat@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-06-21 11:23:02 +0000

    fixed bug with loading jquery
    git-svn-id: https://plugins.svn.wordpress.org/chuck-norris-joke-widget/tags/0.7.1@561700 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ chuck-norris-joke-widget.php
@@ -0,0 +1,102 @@
+<?php
+/**
+ * Plugin Name: Chuck Norris Jokes Widget
+ * Plugin URI: http://maartendecat.be/chuck-norris-jokes-wordpress-plugin/
+ * Description: A widget that shows Chuck Norris jokes on your blog. For personalized Chuck Norris jokes starring yourself, please refer to the Personalized Chuck Norris Jokes Widget.
+ * Version: 0.7
+ * Author: Maarten Decat
+ * Author URI: http://maartendecat.be
+ * License: GPL2
+ *
+ * Copyright 2010  Maarten Decat  (email : mdecat@ulyssis.be)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License, version 2, as 
+ * published by the Free Software Foundation.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+add_action( 'widgets_init', 'load_ChuckNorrisJokeWidget' );
+
+function load_ChuckNorrisJokeWidget() {
+	register_widget( 'ChuckNorrisJokeWidget' );
+	wp_register_script('chuck-norris-jquery', plugins_url('/chuck-norris-joke-widget/jquery.icndb.min.js'), array('jquery'));
+	wp_enqueue_script('chuck-norris-jquery');
+}
+
+/**
+ * The Chuck Norris Widget.
+ */
+class ChuckNorrisJokeWidget extends WP_Widget {
+
+	/**
+	 * Widget setup.
+	 */
+	function ChuckNorrisJokeWidget() {
+		/* Widget settings. */
+		$widget_ops = array( 'classname' => 'chuck-norris-jokes', 'description' => __('A widget that shows Chuck Norris jokes on your blog.', 'chuck-norris-jokes-widget') );
+
+		/* Widget control settings. */
+		$control_ops = array( 'width' => 300, 'height' => 350, 'id_base' => 'chuck-norris-jokes-widget' );
+
+		/* Create the widget. */
+		parent::WP_Widget( 'chuck-norris-jokes-widget', __('Chuck Norris Joke', 'chuck-norris-jokes-widget'), $widget_ops, $control_ops );
+	}
+
+	/**
+	 * How to display the widget on the screen.
+	 */
+	function widget( $args, $instance ) {
+		extract( $args );
+
+		/* Before widget (defined by themes). */
+		echo $before_widget;
+		
+		/* Output the quote */ ?>
+<!-- Chuck Norris Joke Widget plugin -->
+<div id="chuck-norris-joke-widget"></div>
+<script type="text/javascript">
+(function($) {
+$.icndb.client.id = 2;
+$.icndb.client.version = 0.7;
+$(document).ready(function() {
+    $.icndb.getRandomJoke({
+		success: function(joke) {
+			$('#chuck-norris-joke-widget').html(joke.joke);
+		}
+	});
+});
+})(jQuery);
+</script>
+<?php
+		/* After widget (defined by themes). */
+		echo $after_widget;
+	}
+
+	/**
+	 * Update the widget settings.
+	 */
+	function update( $new_instance, $old_instance ) {
+		$instance = $old_instance;
+
+		return $instance;
+	}
+
+	/**
+	 * Displays the widget settings controls on the widget panel.
+	 * Make use of the get_field_id() and get_field_name() function
+	 * when creating your form elements. This handles the confusing stuff.
+	 */
+	function form( $instance ) {
+		
+	}
+}
+
+?>
---
+++ readme.txt
@@ -0,0 +1,75 @@
+=== Plugin Name ===
+Contributors: maarten.decat
+Donate link: http://maartendecat.be
+Tags: chuck norris, jokes, widget, sidebar, fun
+Requires at least: 2.8
+Tested up to: 3.0.4
+Stable tag: 0.7.1
+
+Shows a random Chuck Norris joke on your blog. For personalized Chuck Norris jokes starring yourself, please refer to the Personalized Chuck Norris Jokes Widget.
+
+== Description ==
+
+This plugin adds a small Wordpress widget that shows a random Chuck Norris joke on your blog.
+These jokes are taken from a database on icndb.com.
+For personalized Chuck Norris jokes starring yourself, please refer to the [Personalized Chuck Norris Jokes Widget](http://wordpress.org/extend/plugins/personalized-chuck-norris-joke-widget/ "Personalized Chuck Norris Jokes Widget").
+
+== Installation ==
+
+To install this plugin:
+
+1. Download the plugin and extract the contents.
+1. Upload the entire plugin directory to '/wp-content/plugins/'.
+1. Activate the plugin through the 'Plugins' menu in WordPress.
+1. Drag the "Personalized Chuck Norris Joke" widget in a widget area through the 'Appearance - Widgets' menu.
+1. Press save and close.
+
+You should now see a different Chuck Norris joke on your blog every time the page loads.
+
+== Frequently Asked Questions ==
+
+Have a question? Be sure to ask it at [the plugin homepage](http://www.icndb.com/on-your-website/wordpress/ "Personalized Chuck Norris Jokes Wordpress Plugin Homepage")
+
+== Screenshots ==
+
+1. A Chuck Norris joke.
+
+== Changelog ==
+
+= 0.7.1 =
+
+* Fixed bug with loading jQuery.
+
+= 0.7 =
+
+* Switched to client-side mashup using new jQuery plugin (no caching problems, server load minimized).
+
+= 0.5 =
+
+* Fixed some coding issues
+* Upgraded to new, stable RESTful API
+* Branched off "Personalized Chuck Norris Joke Widget", personalization will only be present in that plugin.
+
+= 0.1.5 =
+
+* Changed to the new icndb.com domain to fetch jokes.
+
+= 0.1 =
+* Basic version: a random joke with possibility to show own first name and last name in the joke (basic replacement).
+
+== Upgrade Notice ==
+
+= 0.7 =
+
+Switched to a client-side mashup using new jQuery plugin. Since jokes are now fetched from the client, there are no more caching problems and server load is minimized.
+
+= 0.5 =
+
+Upgraded to new, stable RESTful API. Also branched off "Personalized Chuck Norris Joke Widget", personalization will only be present in that plugin from now on.
+
+= 0.1.5 =
+
+Changed to the new icndb.com domain to fetch jokes.
+
+= 0.1 =
+Basic version.
